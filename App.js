8ed68f354eaaf3e9f901ea8f1725d35725d31dde
iuliuusource <iuliu.visovan@usource.ro>
2019-12-18 20:52:48 +0200

    some fixes + format + update to 36

--- App.js
+++ App.js
@@ -1,55 +1,71 @@
-import React from "react";
-import { StyleSheet, Text, View, Button, ScrollView, TextInput, Keyboard, AsyncStorage, Platform, Dimensions, AppState, RefreshControl, Alert, TouchableOpacity } from "react-native";
-import { Permissions, Notifications } from "expo";
-import moment from "moment";
-import * as locales from "moment/min/locales";
-moment.locale("ro");
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  ScrollView,
+  TextInput,
+  Keyboard,
+  AsyncStorage,
+  Platform,
+  Dimensions,
+  AppState,
+  RefreshControl,
+  Alert,
+  TouchableOpacity
+} from 'react-native';
+import { Notifications } from 'expo';
+import Permissions from 'expo-permissions';
+import moment from 'moment';
+import * as locales from 'moment/min/locales';
+moment.locale('ro');
 
 const useRemote = true;
-const baseUrl = useRemote ? "https://hey-server.herokuapp.com" : "http://192.168.1.117:3000";
+const baseUrl = useRemote ? 'https://hey-server.herokuapp.com' : 'http://192.168.1.117:3000';
 
 export default class App extends React.Component {
   state = {
-    text: "",
-    targetText: "",
-    registrationNumber: "",
-    targetRegistrationNumber: "",
+    text: '',
+    targetText: '',
+    registrationNumber: '',
+    targetRegistrationNumber: '',
     receivedMessages: [],
     messages: [
       {
         id: 0,
-        text: "Mersi!"
+        text: 'Mersi!'
       },
       {
         id: 1,
-        text: "Scuze!"
+        text: 'Scuze!'
       },
       {
         id: 2,
-        text: "Ai parcat pe locul meu si am nevoie de el. Te rog sa vii sa-ti muti masina cat mai repede!"
+        text: 'Ai parcat pe locul meu si am nevoie de el. Te rog sa vii sa-ti muti masina cat mai repede!'
       },
       {
         id: 3,
-        text: "Ai parcat prost si ii incurci pe ceilalti soferi! Ar fi bine sa-ti muti masina!"
+        text: 'Ai parcat prost si ii incurci pe ceilalti! Ar fi bine sa-ti muti masina!'
       },
       {
         id: 4,
-        text: "Ar trebui sa vii la masina! (ai uitat geamul deschis, cheia in contact, bunuri, etc.)"
+        text: 'Ar trebui sa vii la masina cat mai repede! (ai uitat geamul deschis, politie, cheia in contact, bunuri, etc.)'
       },
       {
         id: 5,
-        text: "Vin acum!"
+        text: 'Vin acum!'
       }
     ],
     appState: AppState.currentState
   };
 
   componentWillUnmount() {
-    AppState.removeEventListener("change", this._handleAppStateChange);
+    AppState.removeEventListener('change', this._handleAppStateChange);
   }
 
   _handleAppStateChange = nextAppState => {
-    if (this.state.appState.match(/inactive|background/) && nextAppState === "active") {
+    if (this.state.appState.match(/inactive|background/) && nextAppState === 'active') {
       setTimeout(() => {
         this.loadMessages();
       }, 500);
@@ -58,20 +74,20 @@ export default class App extends React.Component {
   };
 
   async componentDidMount() {
-    AppState.addEventListener("change", this._handleAppStateChange);
+    AppState.addEventListener('change', this._handleAppStateChange);
 
     // AsyncStorage.clear();
-    const registrationNumber = await AsyncStorage.getItem("registrationNumber");
+    const registrationNumber = await AsyncStorage.getItem('registrationNumber');
     if (registrationNumber) {
       this.setState({ registrationNumber });
       setTimeout(this.loadMessages, 0);
     }
 
-    if (Platform.OS === "android") {
-      Expo.Notifications.createChannelAndroidAsync("messages", {
-        name: "Messages from drivers",
+    if (Platform.OS === 'android') {
+      Notifications.createChannelAndroidAsync('messages', {
+        name: 'Messages from drivers',
         sound: true,
-        priority: "max",
+        priority: 'max',
         vibrate: [0, 250, 250, 1000]
       });
     }
@@ -79,17 +95,17 @@ export default class App extends React.Component {
 
   submit = () => {
     const { text } = this.state;
-    if ((text || "").trim().length < 6) return;
+    if ((text || '').trim().length < 6) return;
     if (/[A-Z]/.test(text) && /[0-9]/.test(text) && !/\s/.test(text)) {
       Alert.alert(
-        "Confirma",
+        'Confirma',
         `${text}\n\nE ok acest numar? \n(va fi folosit de alti soferi ca sa te contacteze)`,
         [
-          { text: "Nu, hopa", onPress: () => {} },
+          { text: 'Nu, hopa', onPress: () => {} },
           {
-            text: "Da",
+            text: 'Da',
             onPress: this.register
-          },
+          }
         ],
         { cancelable: false }
       );
@@ -99,26 +115,26 @@ export default class App extends React.Component {
   register = async () => {
     const { text } = this.state;
     this.setState({ registrationNumber: text });
-    AsyncStorage.setItem("registrationNumber", text);
+    AsyncStorage.setItem('registrationNumber', text);
 
     const { status: existingStatus } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
     let finalStatus = existingStatus;
 
-    if (existingStatus !== "granted") {
+    if (existingStatus !== 'granted') {
       const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
       finalStatus = status;
     }
 
-    if (finalStatus !== "granted") {
+    if (finalStatus !== 'granted') {
       return;
     }
 
     let token = await Notifications.getExpoPushTokenAsync();
 
     await fetch(`${baseUrl}/register`, {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json"
+        'Content-Type': 'application/json'
       },
       body: JSON.stringify({
         registrationNumber: text,
@@ -131,19 +147,22 @@ export default class App extends React.Component {
 
   loadMessages = async () => {
     this.setState({ refreshing: true });
-    const receivedMessages = await (await fetch(`${baseUrl}/my-messages?registrationNumber=${this.state.registrationNumber}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })).json();
-    if (receivedMessages.length) receivedMessages.sort((a, b) => +new Date(b.date) - +new Date(a.date))[0].expanded = true;
+    const receivedMessages = await (
+      await fetch(`${baseUrl}/my-messages?registrationNumber=${this.state.registrationNumber}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+    ).json();
+    if (receivedMessages.length)
+      receivedMessages.sort((a, b) => +new Date(b.date) - +new Date(a.date))[0].expanded = true;
     this.setState({ receivedMessages, refreshing: false });
   };
 
   contactDriver = () => {
     const { targetText } = this.state;
-    if ((targetText || "").trim().length < 6) return;
+    if ((targetText || '').trim().length < 6) return;
     if (/[A-Z]/.test(targetText) && /[0-9]/.test(targetText)) {
       this.setState({ targetRegistrationNumber: targetText });
     }
@@ -157,25 +176,27 @@ export default class App extends React.Component {
   };
 
   sendMessage = async () => {
-    let sentMessages = await AsyncStorage.getItem("sentMessages");
+    let sentMessages = await AsyncStorage.getItem('sentMessages');
     if (!sentMessages) sentMessages = [];
     else sentMessages = JSON.parse(sentMessages);
 
     const { targetRegistrationNumber } = this.state;
 
     const messagesInLastHour = sentMessages.filter(x => +new Date() - +x.dateTimestamp < 1000 * 60 * 60);
-    const messagesInLastHourToThisGuy = messagesInLastHour.filter(x => x.targetRegistrationNumber == targetRegistrationNumber);
+    const messagesInLastHourToThisGuy = messagesInLastHour.filter(
+      x => x.targetRegistrationNumber == targetRegistrationNumber
+    );
 
     if (messagesInLastHourToThisGuy.length > 5 || messagesInLastHour.length > 10) {
-      Alert.alert("Eroare", `Ai trimis prea multe mesaje in ultima vreme.`);
+      Alert.alert('Eroare', `Ai trimis prea multe mesaje in ultima vreme.`);
       return;
     }
     // POST the token to your backend server from where you can retrieve it to send push notifications.
     const message = this.state.messages.find(x => x.selected);
     const response = await fetch(`${baseUrl}/send-message`, {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json"
+        'Content-Type': 'application/json'
       },
       body: JSON.stringify({
         targetRegistrationNumber,
@@ -186,14 +207,14 @@ export default class App extends React.Component {
     Alert.alert(`Mesaj trimis catre ${targetRegistrationNumber}:`, `${message.text}`);
 
     sentMessages.push({ targetRegistrationNumber, message, dateTimestamp: +new Date() });
-    AsyncStorage.setItem("sentMessages", JSON.stringify(sentMessages));
+    AsyncStorage.setItem('sentMessages', JSON.stringify(sentMessages));
     (this.state.messages.find(x => x.selected) || {}).selected = false;
     this.setState({});
   };
 
   cancel = () => {
     (this.state.messages.find(x => x.selected) || {}).selected = false;
-    this.setState({ targetRegistrationNumber: "" });
+    this.setState({ targetRegistrationNumber: '' });
   };
 
   expandMessage = _id => {
@@ -212,9 +233,14 @@ export default class App extends React.Component {
     return (
       <View style={styles.container} onTouchStart={Keyboard.dismiss}>
         {!registrationNumber ? (
-          <View style={{ marginTop: 100, alignItems: "center" }}>
+          <View style={{ marginTop: 100, alignItems: 'center' }}>
             <Text>Introdu numarul de inmatriculare al masinii tale:</Text>
-            <TextInput onChange={event => this.setState({ text: event.nativeEvent.text })} autoCapitalize="characters" style={styles.input} placeholder="e.g. CJ68VGA" />
+            <TextInput
+              onChange={event => this.setState({ text: event.nativeEvent.text })}
+              autoCapitalize="characters"
+              style={styles.input}
+              placeholder="e.g. BZ63VMD"
+            />
             <Button color="#00bcd4" onPress={this.submit} title="Continua" />
           </View>
         ) : (
@@ -229,21 +255,46 @@ export default class App extends React.Component {
                 {!Boolean(targetRegistrationNumber) && (
                   <>
                     <Text>Contacteaza sofer:</Text>
-                    <TextInput onChange={event => this.setState({ targetText: event.nativeEvent.text })} autoCapitalize="characters" style={styles.input} placeholder="e.g. CJ68VDZ" />
-                    <Button color="#00bcd4" disabled={!this.state.targetText} onPress={this.contactDriver} title="Continua" />
+                    <TextInput
+                      onChange={event => this.setState({ targetText: event.nativeEvent.text })}
+                      autoCapitalize="characters"
+                      style={styles.input}
+                      placeholder="e.g. CJ64SVM"
+                    />
+                    <Button
+                      color="#00bcd4"
+                      disabled={!this.state.targetText}
+                      onPress={this.contactDriver}
+                      title="Continua"
+                    />
                     {Boolean(receivedMessages.length) && <Text style={{ marginTop: 25 }}>Mesaje primite:</Text>}
-                    <ScrollView refreshControl={<RefreshControl refreshing={this.state.refreshing} onRefresh={this.loadMessages} />} style={styles.receivedMessagesWrapper}>
+                    <ScrollView
+                      refreshControl={
+                        <RefreshControl refreshing={this.state.refreshing} onRefresh={this.loadMessages} />
+                      }
+                      style={styles.receivedMessagesWrapper}
+                    >
                       {receivedMessages
                         .sort((a, b) => +new Date(b.date) - +new Date(a.date))
                         .map((x, i) => (
-                          <TouchableOpacity activeOpacity={1} onPress={() => this.expandMessage(x._id)} style={styles.receivedMessageWrapper} key={x._id}>
+                          <TouchableOpacity
+                            activeOpacity={1}
+                            onPress={() => this.expandMessage(x._id)}
+                            style={styles.receivedMessageWrapper}
+                            key={x._id}
+                          >
                             <Text style={styles.receivedMessageInfo}>
-                              Acum {moment.duration(moment(x.date).diff(moment())).humanize()} - de la {x.sourceRegistrationNumber}
+                              Acum {moment.duration(moment(x.date).diff(moment())).humanize()} - de la{' '}
+                              {x.sourceRegistrationNumber}
                             </Text>
                             <Text style={styles.receivedMessage}>{x.message}</Text>
                             {x.expanded && (
                               <View style={{ marginTop: 10 }}>
-                                <Button color="#00bcd4" onPress={() => this.replyToDriver(x.sourceRegistrationNumber)} title="Raspunde" />
+                                <Button
+                                  color="#00bcd4"
+                                  onPress={() => this.replyToDriver(x.sourceRegistrationNumber)}
+                                  title="Raspunde"
+                                />
                               </View>
                             )}
                           </TouchableOpacity>
@@ -252,22 +303,34 @@ export default class App extends React.Component {
                   </>
                 )}
                 {Boolean(targetRegistrationNumber) && (
-                  <View style={{ flex: 1, alignItems: "center" }}>
+                  <View style={{ flex: 1, alignItems: 'center' }}>
                     <Text>
                       <Text style={{}}>Trimite un mesaj lui </Text>
-                      <Text style={{ fontSize: 16, fontWeight: "500" }}>{targetRegistrationNumber}</Text>
+                      <Text style={{ fontSize: 16, fontWeight: '500' }}>{targetRegistrationNumber}</Text>
                     </Text>
                     <Text style={{ marginTop: 10 }}>Selecteaza:</Text>
                     <ScrollView style={styles.messagesWrapper}>
                       {messages.map(x => (
                         <TouchableOpacity activeOpacity={1} key={x.id} onPress={() => this.selectMessage(x.id)}>
-                          <Text style={[styles.message, x.selected ? { backgroundColor: "#00bcd4", fontWeight: "500" } : {}]}>{x.text}</Text>
+                          <Text
+                            style={[
+                              styles.message,
+                              x.selected ? { backgroundColor: '#00bcd4', fontWeight: '500' } : {}
+                            ]}
+                          >
+                            {x.text}
+                          </Text>
                         </TouchableOpacity>
                       ))}
                     </ScrollView>
                     <View style={styles.buttonsWrapper}>
                       <Button color="#788190" onPress={this.cancel} title="Inapoi" />
-                      <Button disabled={!this.state.messages.some(x => x.selected)} color="#00bcd4" onPress={this.sendMessage} title="Trimite" />
+                      <Button
+                        disabled={!this.state.messages.some(x => x.selected)}
+                        color="#00bcd4"
+                        onPress={this.sendMessage}
+                        title="Trimite"
+                      />
                     </View>
                   </View>
                 )}
@@ -282,73 +345,73 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   receivedMessagesWrapper: {
-    maxHeight: Dimensions.get("window").height - 250,
-    minHeight: Dimensions.get("window").height - 250,
+    maxHeight: Dimensions.get('window').height - 250,
+    minHeight: Dimensions.get('window').height - 250,
     borderWidth: 1,
-    borderColor: "#00bcd4"
+    borderColor: '#00bcd4'
   },
   receivedMessageWrapper: {
     padding: 15,
-    borderColor: "#788190",
+    borderColor: '#788190',
     borderBottomWidth: 0.6
   },
   receivedMessageInfo: {},
   receivedMessage: {
-    fontWeight: "500",
-    color: "#677080",
+    fontWeight: '500',
+    color: '#677080',
     fontSize: 18,
     marginVertical: 1
   },
   buttonsWrapper: {
-    position: "absolute",
+    position: 'absolute',
     bottom: 0,
     padding: 30,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    width: "100%"
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%'
   },
   message: {
-    backgroundColor: "#78819077",
+    backgroundColor: '#78819077',
     paddingHorizontal: 10,
     paddingVertical: 10,
     borderRadius: 1,
-    textAlign: "center",
+    textAlign: 'center',
     marginTop: 10,
     marginHorizontal: 15,
     fontSize: 16,
-    color: "white"
+    color: 'white'
   },
   messagesWrapper: {
-    maxHeight: Dimensions.get("window").height - 200,
-    minHeight: Dimensions.get("window").height - 200,
-    borderColor: "#00bcd4",
+    maxHeight: Dimensions.get('window').height - 200,
+    minHeight: Dimensions.get('window').height - 200,
+    borderColor: '#00bcd4',
     marginVertical: 25,
     marginBottom: 50
   },
   messagesWrapperContainer: {
-    alignItems: "center"
+    alignItems: 'center'
   },
   accessedPageWrapper: {
-    justifyContent: "space-between",
+    justifyContent: 'space-between',
     flex: 1
   },
   bottomWrapper: {
-    position: "absolute",
+    position: 'absolute',
     bottom: 15,
     left: 15
   },
   topWrapper: {
-    alignItems: "center",
+    alignItems: 'center',
     paddingTop: 35
   },
   registrationNumber: {
     fontSize: 18,
     marginTop: 5,
-    fontWeight: "500"
+    fontWeight: '500'
   },
   input: {
     borderWidth: 1,
-    borderColor: "#788190",
+    borderColor: '#788190',
     padding: 10,
     paddingHorizontal: 15,
     marginVertical: 10,
@@ -357,7 +420,7 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center"
+    backgroundColor: '#fff',
+    alignItems: 'center'
   }
 });
